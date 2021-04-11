36c3b836d4c4fc4a5eefa800c4ef6acedfc5fbb6
Fiorella <fiorella2994s@gmail.com>
2021-04-11 11:17:02 -0300

    Changes made to html and js register validation.

--- week9/js/register.js
+++ week9/js/register.js
@@ -109,27 +109,28 @@ function submitRegisterForm(e) {
 function resetRegisterForm(e) {
     registerForm.reset();
 }
-var myInput = document.getElementById("psw");
+/*Variables for validation of password*/
+var formRegisterFirstPassword = document.getElementById("psw");
 var letter = document.getElementById("letter");
 var capital = document.getElementById("capital");
 var number = document.getElementById("number");
 var length = document.getElementById("length");
 
 // When the user clicks on the password field, show the message box
-myInput.onfocus = function() {
+formRegisterFirstPassword.onfocus = function() {
   document.getElementById("message").style.display = "block";
 }
 
 // When the user clicks outside of the password field, hide the message box
-myInput.onblur = function() {
+formRegisterFirstPassword.onblur = function() {
   document.getElementById("message").style.display = "none";
 }
 
 // When the user starts to type something inside the password field
-myInput.onkeyup = function() {
+formRegisterFirstPassword.onkeyup = function() {
   // Validate lowercase letters
   var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) {
+  if(formRegisterFirstPassword.value.match(lowerCaseLetters)) {
     letter.classList.remove("invalid");
     letter.classList.add("valid");
   } else {
@@ -139,7 +140,7 @@ myInput.onkeyup = function() {
 
   // Validate capital letters
   var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) {
+  if(formRegisterFirstPassword.value.match(upperCaseLetters)) {
     capital.classList.remove("invalid");
     capital.classList.add("valid");
   } else {
@@ -149,7 +150,7 @@ myInput.onkeyup = function() {
 
   // Validate numbers
   var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) {
+  if(formRegisterFirstPassword.value.match(numbers)) {
     number.classList.remove("invalid");
     number.classList.add("valid");
   } else {
@@ -158,7 +159,7 @@ myInput.onkeyup = function() {
   }
 
   // Validate length
-  if(myInput.value.length >= 8) {
+  if(formRegisterFirstPassword.value.length >= 8) {
     length.classList.remove("invalid");
     length.classList.add("valid");
   } else {
